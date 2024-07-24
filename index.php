af7c68ce07ed36964428c1c027849612774a5ba0
Oleksandra Ubska <alessandraubska@gmail.com>
2024-07-24 14:49:11 +0200

    feat: add table styles

--- index.php
+++ index.php
@@ -47,6 +47,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Hotel</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="container">
