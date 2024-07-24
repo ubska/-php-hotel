a5818d984f43038f097d81c4fd1faff7a3ba34aa
Oleksandra Ubska <alessandraubska@gmail.com>
2024-07-24 15:25:40 +0200

    feat: loop forEach

--- index.php
+++ index.php
@@ -1,45 +1,42 @@
 <?php
     // informazioni degli hotel
     $hotels = [
-
             [
                 'name' => 'Hotel Belvedere',
                 'description' => 'Hotel Belvedere Descrizione',
                 'parking' => true,
                 'vote' => 4,
-                'distance_to_center' => 10.4
+                'distance_to_center' => 10.4,
             ],
             [
                 'name' => 'Hotel Futuro',
                 'description' => 'Hotel Futuro Descrizione',
                 'parking' => true,
                 'vote' => 2,
-                'distance_to_center' => 2
+                'distance_to_center' => 2,
             ],
             [
                 'name' => 'Hotel Rivamare',
                 'description' => 'Hotel Rivamare Descrizione',
                 'parking' => false,
                 'vote' => 1,
-                'distance_to_center' => 1
+                'distance_to_center' => 1,
             ],
             [
                 'name' => 'Hotel Bellavista',
                 'description' => 'Hotel Bellavista Descrizione',
                 'parking' => false,
                 'vote' => 5,
-                'distance_to_center' => 5.5
+                'distance_to_center' => 5.5,
             ],
             [
                 'name' => 'Hotel Milano',
                 'description' => 'Hotel Milano Descrizione',
                 'parking' => true,
                 'vote' => 2,
-                'distance_to_center' => 50
+                'distance_to_center' => 50,
             ],
-
         ];
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -55,6 +52,7 @@
 
         <!-- tabella -->
          <table>
+            <!-- intestazione della tabella -->
             <thead>
                 <tr>
                     <th>Name</th>
@@ -64,14 +62,17 @@
                     <th>Distance of center</th>
                 </tr>
             </thead>
+            <!-- corpo della tabella -->
             <tbody>
+                <?php foreach($hotels as $hotel) { ?>
                 <tr>
-                    <td>Name</td>
-                    <td>Desctiption</td>
-                    <td>Parcking</td>
-                    <td>Vote</td>
-                    <td>Distance of center</td>
+                    <td><?php echo $hotel['name']; ?></td>
+                    <td><?php echo $hotel['description']; ?></td>
+                    <td><?php echo $hotel['parking'] ? 'Si' : 'No';?></td>
+                    <td><?php echo $hotel['vote']; ?></td>
+                    <td><?php echo $hotel['distance_to_center']; ?></td>
                 </tr>
+                <?php } ?>
             </tbody>
          </table>
     </div>
